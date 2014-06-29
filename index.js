9c81a140d548b6a73bb9448a09e696437dd70fda
jgleesawn <j.gleesawn@gmail.com>
2014-06-29 12:08:44 -0800

    Added add/update User/Quest javascript interface.

--- index.js
+++ index.js
@@ -20,11 +20,69 @@ function getallUsers(){
 function getallQuests(){ 
 	ws.send("get all Quests; a");
 };
+function prepUser(){
+	uname = '"'+document.getElementById('username').value+'"'
+	ufname = '"'+document.getElementById('firstname').value+'"'
+	ulname = '"'+document.getElementById('lastname').value+'"'
+	str = '{"Username":'+uname+',"Firstname":'+ufname+',"Lastname":'+ulname+',"Xp":0,"Completedquests":[0],"Attributes":[""]}'
+	return str
+}
+function AddUser(){
+	ws.send("add User;"+prepUser())
+}
+function UpdateUser(){
+	ws.send("update User;"+prepUser())
+}
+function prepQuest(){
+	qid = parseInt(document.getElementById('qid').value)
+	qname = '"'+document.getElementById('questname').value+'"'
+	qdesc = '"'+document.getElementById('description').value+'"'
+	qcat = '"'+document.getElementById('category').value+'"'
+	qrec = document.getElementById('recurringtrue').value == 'true' ? true : false
+	qxp = parseInt(document.getElementById('Xp').value)
+	qreq = '['+reqquest+']'
+	qattr = '['+questattr+']'
+
+	str = '{"Questid":'+qid+',"Name":'+qname+',"Description":'+qdesc+',"Category":'+qcat+',"Recurring":'+qrec+',"Xp":'+qxp+',"Requiredquests":'+qreq+',"Attributes":'+qattr+'}'
+	return str
+}
+function AddQuest(){
+	ws.send("add Quest;"+str)
+}
+function UpdateQuest(){
+	ws.send("update Quest;"+str)
+}
+function AppendQuestReq() {
+	var elem = document.getElementById("reqquest")
+	reqquest.push(parseInt(elem.value))
+}
+function AppendAttribute() {
+	var elem = document.getElementById("attribute")
+	questattr.push('"'+elem.value+'"')
+}
+var questattr = []
+var reqquest = [0]
 </script>
 <div id='input'>
-name:<input type='text' id='name' name='name' value='oldman'>
-age:<input type='text' id='age' name='age' value='132'>
-<button onclick='get()'>Get</button>
+User: <br>
+username<input type='text' id='username' name='username' value='username'><br>
+firstname<input type='text' id='firstname' name='firstname' value='firstname'><br>
+lasname<input type='text' id='lastname' name='lastname' value='lastname'><br>
+<button onclick='AddUser()'>Add User</button><button onclick='UpdateUser()'>Update User</button><br>
+
+Quest: <br>
+qid<input type='number' id='qid' name='qid' min='0' max='10000'> <br>
+questname<input type='text' id='questname' name='questname' value='questname'><br>
+description<input type='text' id='description' name='description' value='description'><br>
+category<input type='text' id='category' name='category' value='category'><br>
+recurring<br><input type='radio' id='recurringtrue' name='recurring' value='true'>true<br>
+<input type='radio' id='recurringfalse' name='recurring' value='false'>false<br>
+xp<input type='number' id='Xp' name='Xp' min='0' max='10000'> <br>
+add required quest<input type='number' id='reqquest' name='reqquest' min='0' max='10000'><button onclick='AppendQuestReq()'>Add Req Quest </button><br>
+add attribute<input type='text' id='attribute' name='attribute' value='attribute'><button onclick='AppendAttribute()'>Add Attribute</button><br>
+<button onclick='AddQuest()'>Add Quest</button><button onclick='UpdateQuest()'>Update Quest</button> <br>
+
+
 <button onclick='store()'>Store</button>
 <button onclick='getallUsers()'>All Users</button>
 <button onclick='getallQuests()'>All Quests</button>
